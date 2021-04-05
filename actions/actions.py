fa5b6b85a060ef99b61760c3f9e3c9a8a89155ad
sanatnayar <snaya091@uottawa.ca>
2021-04-05 12:47:29 -0400

    added action server

--- actions/actions.py
+++ actions/actions.py
@@ -16,8 +16,12 @@ import pprint
 import datetime
 import time
 from datetime import datetime, timedelta 
+import requests
+import textdistance
+
 client = MongoClient("mongodb+srv://QuickPassBoys:bigchungus123@cluster0.jsf18.mongodb.net/QuickPass?retryWrites=true&w=majority")
 db = client.QuickPass
+
 class aggregate(Action):
 
     def name(self) -> Text:
@@ -58,10 +62,37 @@ class aggregate(Action):
             for alert in alerts.find({"time": {"$gte": prev_time, "$lt": current_time}}):
                 ct+=1
                 n.append(alert)
-                dispatcher.utter_message("Alert "+str(ct)+": "+"Time -> "+str(alert["time"])+", Type -> "+str(alert["type"]))
+                dispatcher.utter_message("Alert "+str(ct)+": "+" \n Time -> "+str(alert["time"])+", \n Type -> "+str(alert["type"]))
             pprint.pprint(len(n))
             message = str(len(n)) + " alert(s) have occured in the last " + time1+ " hour(s)"
             dispatcher.utter_message(message)
+        
+        dispatcher.utter_message()
+        return [SlotSet("mongo_type", None)]
+
+class action_Pop(Action):
+
+    def name(self) -> Text:
+        return "action_Pop"
+
+    async def run(
+        self, dispatcher, tracker: Tracker, domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+
+        pop = tracker.get_slot("pop_type")
+    
+        print(pop)
+        fac_info = requests.get('http://quickpass-backend.azurewebsites.net/facility/by/602ea8d423a00b4812b77ee6')
+        fac = fac_info.json()
+        isCapacitySet = fac['isCapacitySet']
+        capacity = fac['capacity']
+        print(capacity)
+        currentCapacity = fac['currentCapacity']
+        
+        if pop == "max" or pop == "maximum" or textdistance.hamming(pop, "maximum")<2:
+            dispatcher.utter_message("The maximum capacity set for this facility is: "+str(capacity)+" individuals")
+        elif pop == "current" or pop=="currently" or textdistance.hamming(pop, "current")<2:
+            dispatcher.utter_message("The current load of the facility is "+str(currentCapacity)+" individuals")
         
         dispatcher.utter_message()
         return [SlotSet("mongo_type", None)]
